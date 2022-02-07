461fff889919a22eefbb5a8b99f736d7c75e9cc9
Kacper Debowski <kacper@kaholo.io>
2022-02-07 17:11:07 +0100

    Added dynamic region list and fixed access key being string

--- autocomplete.js
+++ autocomplete.js
@@ -1,6 +1,7 @@
 const parsers = require("./parsers");
 const CodeBuildService = require('./aws.codebuild.service');
 const MAX_RESULTS = 10;
+const MISSING_OR_INCORRECT_CREDENTIALS_ERROR = "Missing or incorrect credentials - please select valid access and secret keys first";
 
 // auto complete helper methods
 
@@ -73,7 +74,7 @@ async function listBuildsAuto(query, pluginSettings, triggerParameters){
   const items = filterItems(result.ids.map(buildId => getAutoResult(buildId)), query);
   while (items.length < MAX_RESULTS && result.nextToken){
     result = await client.listBuilds({
-      project: params.project, 
+      project: params.project,
       nextToken: result.nextToken
     });
     items.push(...filterItems(result.ids.map(buildId => getAutoResult(buildId)), query));
@@ -81,8 +82,36 @@ async function listBuildsAuto(query, pluginSettings, triggerParameters){
   return items;
 }
 
+async function listRegions(query, pluginSettings, actionParams) {
+  let [  settings, params  ] = [ mapAutoParams(pluginSettings), mapAutoParams(actionParams) ];
+  params = { ...params, region: params.region || "eu-west-2" };
+  const client = CodeBuildService.from(params, settings);
+
+  const ec2RegionsPromise = client.listRegions();
+  const lightsailRegionsPromise = client.lightsail.getRegions().promise();
+
+  return Promise.all([ec2RegionsPromise, lightsailRegionsPromise]).then(
+      ([ec2Regions, lightsailRegions]) => {
+        return ec2Regions.map((ec2Region) => {
+          const lsRegion = lightsailRegions.regions.find((x) => x.name === ec2Region.RegionName);
+          return lsRegion ?
+              { id: ec2Region.RegionName, value: `${ec2Region.RegionName} (${lsRegion.displayName})` } :
+              { id: ec2Region.RegionName, value: ec2Region.RegionName }
+        }).sort((a,b) => {
+          if (a.value > b.value) return 1;
+          if (a.value < b.value) return -1;
+          return 0;
+        });
+      }
+  ).catch((err) => {
+    console.error(err);
+    throw MISSING_OR_INCORRECT_CREDENTIALS_ERROR;
+  });
+}
+
+
 module.exports = {
-  listRegionsAuto: listAuto("listRegions", ["RegionName"]),
+  listRegions,
   listProjectsAuto,
   listBuildsAuto
 }
