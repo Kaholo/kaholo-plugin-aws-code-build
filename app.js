70bfa6887820041b52c12633d10a9f2d51876df6
maciek <42122011+xkcm@users.noreply.github.com>
2022-07-12 11:33:56 +0200

    KP-839 Implement filtered builds autocomplete, add Status parameter to List Builds method (#7)

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const awsPluginLibrary = require("kaholo-aws-plugin-library");
-const _ = require("lodash");
 const AWS = require("aws-sdk");
-const { fetchRecursively } = require("./helpers");
+
 const autocomplete = require("./autocomplete");
 const payloadFunctions = require("./payload-functions");
+const {
+  listBuilds,
+  listProjects,
+} = require("./code-commit-list-functions");
 
 const simpleAwsMethods = {
   startBuild: awsPluginLibrary.generateAwsMethod("startBuild", payloadFunctions.prepareStartBuildPayload),
@@ -14,56 +17,6 @@ const simpleAwsMethods = {
   getProject: awsPluginLibrary.generateAwsMethod("batchGetProjects", payloadFunctions.prepareGetProjectPayload),
   deleteProject: awsPluginLibrary.generateAwsMethod("deleteProject", payloadFunctions.prepareDeleteProjectPayload),
 };
-
-async function listProjects(codeBuildClient, params) {
-  const payload = payloadFunctions.prepareListProjectsPayload(params);
-
-  const projects = await fetchRecursively(
-    codeBuildClient,
-    {
-      methodName: "listProjects",
-      outputDataPath: "projects",
-    },
-    payload,
-  ).catch((error) => {
-    throw new Error(`Failed to list projects: ${error.message || JSON.stringify(error)}`);
-  });
-
-  return { projects };
-}
-
-async function listBuilds(codeBuildClient, params) {
-  const payload = payloadFunctions.prepareListBuildsPayload(params);
-
-  let resultPromise;
-  if (payload.projectName) {
-    resultPromise = fetchRecursively(
-      codeBuildClient,
-      {
-        methodName: "listBuildsForProject",
-        outputDataPath: "ids",
-      },
-      payload,
-    ).catch((error) => {
-      throw new Error(`Failed to list builds: ${error.message || JSON.stringify(error)}`);
-    });
-  } else {
-    resultPromise = fetchRecursively(
-      codeBuildClient,
-      {
-        methodName: "listBuilds",
-        outputDataPath: "ids",
-      },
-      _.omit(payload, "projectName"),
-    ).catch((error) => {
-      throw new Error(`Failed to list builds: ${error.message || JSON.stringify(error)}`);
-    });
-  }
-
-  const builds = await resultPromise;
-
-  return { builds };
-}
 
 module.exports = awsPluginLibrary.bootstrap(
   AWS.CodeBuild,
--- autocomplete.js
+++ autocomplete.js
@@ -1,11 +1,8 @@
 const awsPluginLibrary = require("kaholo-aws-plugin-library");
+const { listBuilds } = require("./code-commit-list-functions");
 const { fetchRecursively } = require("./helpers");
 
-function createAwsAutocompleteFunction(
-  methodName,
-  outputDataPath,
-  [valuePath, labelPath] = [],
-) {
+function createAwsAutocompleteFunction(methodName, outputDataPath) {
   return async (query, params, awsClient) => {
     const fetchResult = await fetchRecursively(awsClient, {
       methodName,
@@ -14,20 +11,31 @@ function createAwsAutocompleteFunction(
       throw new Error(`Failed to list ${outputDataPath.toLowerCase()}: ${error.message || JSON.stringify(error)}`);
     });
 
-    const mappedAutocompleteItems = fetchResult.map((fetchedItem) => {
-      const autocompleteValue = valuePath ? fetchedItem[valuePath] : fetchedItem;
-      const autocompleteLabel = labelPath ? fetchedItem[labelPath] : autocompleteValue;
-      return awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(
-        autocompleteValue,
-        autocompleteLabel,
-      );
-    });
+    const mappedAutocompleteItems = fetchResult.map((fetchedItem) => (
+      awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(fetchedItem)
+    ));
 
     return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedAutocompleteItems, query);
   };
 }
 
+function createListBuildsAutocomplete(status) {
+  return async (query, params, codeBuildClient) => {
+    const { builds } = await listBuilds(codeBuildClient, {
+      project: params.project,
+      status,
+    });
+
+    const mappedBuilds = builds.map((build) => (
+      awsPluginLibrary.autocomplete.toAutocompleteItemFromPrimitive(build.id)
+    ));
+
+    return awsPluginLibrary.autocomplete.filterItemsByQuery(mappedBuilds, query);
+  };
+}
+
 module.exports = {
   listProjectsAuto: createAwsAutocompleteFunction("listProjects", "projects"),
-  listBuildsAuto: createAwsAutocompleteFunction("listBuilds", "ids"),
+  listBuildsAuto: createListBuildsAutocomplete("ALL"),
+  listBuildsInProgressAuto: createListBuildsAutocomplete("IN_PROGRESS"),
 };
